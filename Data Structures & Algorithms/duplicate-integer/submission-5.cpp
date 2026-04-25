6049849c5b1b468480f68cc521f2edbdf4be05df
Praneeth Chamala <91653674+cjspraneeth@users.noreply.github.com>
2026-04-25 11:02:57 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(auto x : nums)
+        {
+            m[x]++;
+            if(m[x]>1){
+                return true;
+            }
+        }
+        return false;
+        // testing post making repo public
+
+        
+    }
+};

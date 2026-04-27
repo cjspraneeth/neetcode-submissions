140eefcc284fe3bb96aa430b21d5f247342ed824
Praneeth Chamala <91653674+cjspraneeth@users.noreply.github.com>
2026-04-27 23:04:36 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // no extra set storing as its only one valid pair you always have the index in the repeating case as the newer one gets overwritten
+        unordered_map<int,int>indexMap; 
+        vector<int>ans;
+        for (int i =0 ;i<nums.size();i++){
+            indexMap[nums[i]]=i;
+        }
+        for (int i =0 ;i<nums.size();i++){
+           
+            auto it = indexMap.find(target-nums[i]);
+            if (it!= indexMap.end()  && it->second!=i){
+                ans.push_back(i);
+                ans.push_back(it->second);
+                // sort(ans.begin(),ans.end());
+                return ans;
+            }
+            
+            
+        }
+    }
+};

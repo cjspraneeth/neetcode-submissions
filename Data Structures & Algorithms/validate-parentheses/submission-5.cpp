e14a65806419a4b45c73d691253b88bb7b87d46e
Praneeth Chamala <91653674+cjspraneeth@users.noreply.github.com>
2026-04-29 21:55:40 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isopen(char c){
+        if (c=='(' || c=='{' || c=='['){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool isValid(string s) {
+        stack<char>store;
+        unordered_map<char,char>m;
+        m['}']='{';
+        m[']']='[';
+        m[')']='(';
+        for(auto x : s){
+            if(isopen(x)){
+                store.push(x);
+            }
+            else{
+                if(store.size()>0 && m[x]==store.top()){
+                    store.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(store.size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+        
+        
+        
+    }
+};
